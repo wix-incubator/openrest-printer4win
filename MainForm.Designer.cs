4921adb5b87ef7e1f0126419cb001dcadaee2f4f
3LOK <yoavamit@yahoo.com>
2014-08-28 06:46:50 -0700

    first version

---
+++ MainForm.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace OpenRest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.loginpanel = new System.Windows.Forms.Panel();
+            this.loginError = new System.Windows.Forms.Label();
+            this.Connect = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.close = new System.Windows.Forms.Label();
+            this.loggedinpanel = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ConfigurePrinter = new System.Windows.Forms.Label();
+            this.ConnectionStatus = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.OpenSite = new System.Windows.Forms.Button();
+            this.loginpanel.SuspendLayout();
+            this.loggedinpanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.BalloonTipTitle = "OpenRest";
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "OpenRest";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.Click += new System.EventHandler(this.notifyIcon_Click);
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // loginpanel
+            // 
+            this.loginpanel.Controls.Add(this.loginError);
+            this.loginpanel.Controls.Add(this.Connect);
+            this.loginpanel.Controls.Add(this.label2);
+            this.loginpanel.Controls.Add(this.textBox2);
+            this.loginpanel.Controls.Add(this.label1);
+            this.loginpanel.Controls.Add(this.textBox1);
+            this.loginpanel.Location = new System.Drawing.Point(12, 12);
+            this.loginpanel.Name = "loginpanel";
+            this.loginpanel.Size = new System.Drawing.Size(376, 211);
+            this.loginpanel.TabIndex = 0;
+            // 
+            // loginError
+            // 
+            this.loginError.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginError.ForeColor = System.Drawing.Color.White;
+            this.loginError.Location = new System.Drawing.Point(3, 148);
+            this.loginError.Name = "loginError";
+            this.loginError.Size = new System.Drawing.Size(361, 16);
+            this.loginError.TabIndex = 5;
+            this.loginError.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Connect
+            // 
+            this.Connect.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Connect.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.Connect.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.Connect.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(0)))), ((int)(((byte)(33)))));
+            this.Connect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Connect.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Connect.ForeColor = System.Drawing.Color.White;
+            this.Connect.Location = new System.Drawing.Point(114, 169);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(234, 39);
+            this.Connect.TabIndex = 4;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(13, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(114, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(234, 27);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(11, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(114, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // close
+            // 
+            this.close.AutoSize = true;
+            this.close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.close.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close.ForeColor = System.Drawing.Color.White;
+            this.close.Location = new System.Drawing.Point(360, 0);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(24, 23);
+            this.close.TabIndex = 1;
+            this.close.Text = "_";
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // loggedinpanel
+            // 
+            this.loggedinpanel.Controls.Add(this.OpenSite);
+            this.loggedinpanel.Controls.Add(this.label4);
+            this.loggedinpanel.Controls.Add(this.ConfigurePrinter);
+            this.loggedinpanel.Controls.Add(this.ConnectionStatus);
+            this.loggedinpanel.Location = new System.Drawing.Point(8, 8);
+            this.loggedinpanel.Name = "loggedinpanel";
+            this.loggedinpanel.Size = new System.Drawing.Size(376, 215);
+            this.loggedinpanel.TabIndex = 1;
+            this.loggedinpanel.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(283, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Disconnect";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // ConfigurePrinter
+            // 
+            this.ConfigurePrinter.AutoSize = true;
+            this.ConfigurePrinter.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ConfigurePrinter.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConfigurePrinter.ForeColor = System.Drawing.Color.White;
+            this.ConfigurePrinter.Location = new System.Drawing.Point(168, 184);
+            this.ConfigurePrinter.Name = "ConfigurePrinter";
+            this.ConfigurePrinter.Size = new System.Drawing.Size(105, 13);
+            this.ConfigurePrinter.TabIndex = 3;
+            this.ConfigurePrinter.Text = "Configure Printer";
+            this.ConfigurePrinter.Click += new System.EventHandler(this.ConfigurePrinter_Click);
+            // 
+            // ConnectionStatus
+            // 
+            this.ConnectionStatus.AutoSize = true;
+            this.ConnectionStatus.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConnectionStatus.ForeColor = System.Drawing.Color.White;
+            this.ConnectionStatus.Location = new System.Drawing.Point(13, 77);
+            this.ConnectionStatus.Name = "ConnectionStatus";
+            this.ConnectionStatus.Size = new System.Drawing.Size(106, 18);
+            this.ConnectionStatus.TabIndex = 0;
+            this.ConnectionStatus.Text = "Connection:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::OpenRest.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(388, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // OpenSite
+            // 
+            this.OpenSite.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.OpenSite.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OpenSite.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OpenSite.ForeColor = System.Drawing.Color.White;
+            this.OpenSite.Location = new System.Drawing.Point(16, 114);
+            this.OpenSite.Name = "OpenSite";
+            this.OpenSite.Size = new System.Drawing.Size(336, 52);
+            this.OpenSite.TabIndex = 5;
+            this.OpenSite.Text = "Open Management Site";
+            this.OpenSite.UseVisualStyleBackColor = true;
+            this.OpenSite.Click += new System.EventHandler(this.OpenSite_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(0)))), ((int)(((byte)(33)))));
+            this.ClientSize = new System.Drawing.Size(390, 230);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.loggedinpanel);
+            this.Controls.Add(this.loginpanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OpenRest";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.loginpanel.ResumeLayout(false);
+            this.loginpanel.PerformLayout();
+            this.loggedinpanel.ResumeLayout(false);
+            this.loggedinpanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Panel loginpanel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label close;
+        private System.Windows.Forms.Panel loggedinpanel;
+        private System.Windows.Forms.Label ConnectionStatus;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label ConfigurePrinter;
+        private System.Windows.Forms.Label loginError;
+        private System.Windows.Forms.Button OpenSite;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Timers;
+using Microsoft.Win32;
+using com.openrest.v1_1;
+using System.Diagnostics;
+using System.Threading;
+
+namespace OpenRest
+{
+    public partial class MainForm : Form
+    {
+        private Boolean loggedIn = false;
+        private Boolean connectionStatus;
+        private Boolean printing = false;
+        private System.Timers.Timer refreshTimer;
+        private String accessToken = null;
+
+        private static string REGISTRY_NAME = "HKEY_CURRENT_USER\\OpenRest";
+        private static string USERNAME = "username";
+        private static string PASSWORD = "password";
+        private static string PRINTED = "printed";
+       // private static string REGISTRY_PRINTER_NAME = "PrinterName";
+
+        private WebBrowser browser = new WebBrowser();
+        private com.openrest.v1_1.OpenrestClient client = new com.openrest.v1_1.OpenrestClient(new System.Uri("https://api.openrest.com/v1.1"));
+
+        private static readonly DateTime EPOCH = new DateTime(1970,1,1,0,0,0);
+
+        private List<String> printed = new List<String>();
+        private List<Order> toPrint = new List<Order>();
+        private long ordersSince = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // Setup refresh timer
+            refreshTimer = new System.Timers.Timer(2000);
+            refreshTimer.Elapsed += new ElapsedEventHandler(refreshTimer_Elapsed);
+
+            browser.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(browser_DocumentCompleted);
+            // Setup list of printers
+            /*foreach (string printer in System.Drawing.Printing.PrinterSettings.InstalledPrinters)
+            {
+                Printers.Items.Add(printer);
+            }
+
+            Printers.SelectedItem = Registry.GetValue(REGISTRY_NAME, REGISTRY_PRINTER_NAME, "");
+            System.Drawing.Printing.PrinterSettings.*/
+            String s = (String)Registry.GetValue(REGISTRY_NAME, PRINTED, "");
+            printed = s.Split(new char[]{','}).ToList();
+        }
+
+        void refreshTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (!loggedIn) return;
+
+            this.BeginInvoke(new MethodInvoker(delegate()
+            {
+                QueryOrdersRequest request = new QueryOrdersRequest();
+
+                request.accessToken = accessToken;
+                request.distributorId = "us.openrest.com";
+                if (ordersSince == 0)
+                {
+                    request.limit = 10;
+                }
+                else
+                {
+                    request.since = ordersSince;
+                }
+
+                request.status = "new";
+                request.fields = new List<String>{"id", "status", "modified", "locale"};
+
+                OrdersResponse response;
+                try
+                {
+                    response = client.Request<OrdersResponse>(request);
+                }
+                catch (OpenrestException e2)
+                {
+                    ConnectionStatus.Text = "Connection: × (" + e2.ErrorMessage + ")";
+                    connectionStatus = false;
+                    notifyIcon.ShowBalloonTip(5000, "OpenRest", "Connection Error! "+e2.ErrorMessage, ToolTipIcon.Error);
+                    return;
+                }
+                catch (Exception e2)
+                {
+                    ConnectionStatus.Text = "Connection: ×";
+                    connectionStatus = false;
+                    notifyIcon.ShowBalloonTip(5000, "OpenRest", "Connection Error!", ToolTipIcon.Error);
+                    return;
+                }
+                ConnectionStatus.Text = "Connection: ✓";
+                if (!connectionStatus)
+                {
+                    notifyIcon.ShowBalloonTip(5000, "OpenRest", "Connected.", ToolTipIcon.Info);
+                }
+                connectionStatus = true;
+
+                foreach (Order order in response.results)
+                {
+                    long orderModified = order.modified.Value;
+
+                    if (orderModified > ordersSince)
+                    {
+                        ordersSince = orderModified + 1;
+                    }
+
+                    toPrint.Add(order);
+                }
+
+                printNext();
+            }));
+        }
+
+        private void printNext()
+        {
+            if (toPrint.Count == 0) return;
+            if (printing) return;
+
+            printing = true;
+
+            Order order = toPrint[0];
+            toPrint.RemoveAt(0);
+
+            if (printed.Contains(order.id))
+            {
+                printing = false;
+                printNext();
+                return;
+            }
+
+            notifyIcon.ShowBalloonTip(5000, "OpenRest", "Printing: "+order.id, ToolTipIcon.Info);
+            GetOrderRequest request = new GetOrderRequest();
+            request.fields = new List<String> { "id", "restaurantId", "created", "submitAt", "properties", "orderItems", "received", "user", "delivery", "contact", "platform", "source", "developer", "payments", "price", "ref", "status", "modified", "locale", "html" };
+            request.accessToken = accessToken;
+            request.orderId = order.id;
+            request.viewMode = "restaurant";
+            request.anonymize = true;
+            request.printCsc = false;
+            request.locale = order.locale;
+            request.printHeader = true;
+            request.embed = true;
+            request.printConfirmation = false;
+
+            try
+            {
+                order = client.Request<Order>(request);
+            }
+            catch (OpenrestException e2)
+            {
+                ConnectionStatus.Text = "Connection: × (" + e2.ErrorMessage + ")";
+                connectionStatus = false;
+
+                notifyIcon.ShowBalloonTip(5000, "OpenRest", "Print Error! " + e2.ErrorMessage, ToolTipIcon.Error);
+
+                toPrint.Add(order);
+                delayPrintNext();
+                return;
+            }
+            catch (Exception e2)
+            {
+                ConnectionStatus.Text = "Connection: ×";
+                connectionStatus = false;
+
+                notifyIcon.ShowBalloonTip(5000, "OpenRest", "Print Error! "+e2.Message, ToolTipIcon.Error);
+
+                toPrint.Add(order); 
+                printing = false;
+                delayPrintNext();
+                return;
+            }
+
+            browser.DocumentText = order.html;
+            printed.Add(order.id);
+            Registry.SetValue(REGISTRY_NAME, PRINTED, string.Join(",", printed.ToArray()));
+        }
+
+        void delayPrintNext()
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += delegate
+            {
+                Thread.Sleep(5000);
+                this.BeginInvoke(new MethodInvoker(delegate()
+               {
+                   printing = false;
+                   printNext();
+               }));
+            };
+            worker.RunWorkerAsync();
+        }
+
+        void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            ((WebBrowser)sender).Print();
+            printing = false;
+            printNext();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+
+            worker.DoWork += delegate
+            {
+                this.BeginInvoke(new MethodInvoker(delegate()
+                {
+                    String username = (String)Registry.GetValue(REGISTRY_NAME, USERNAME, "");
+                    String password = (String)Registry.GetValue(REGISTRY_NAME, PASSWORD, "");
+
+                    textBox1.Text = username;
+                    textBox2.Text = password;
+
+                    if ((username != "") && (password != ""))
+                    {
+                        Connect_Click(null, null);
+                    }
+                    else
+                    {
+                        this.WindowState = FormWindowState.Normal;
+                        Show();
+                    }
+                }));
+            };
+
+            worker.RunWorkerAsync();
+
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == WindowState) {
+                Hide();
+            }
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void notifyIcon_Click(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+            GetRolesRequest request = new GetRolesRequest();
+            
+            accessToken = "spice|" + username + "|" + password;
+            request.accessToken = accessToken;
+            loginError.Text = "";
+
+            notifyIcon.ShowBalloonTip(5000, "OpenRest", "Connecting...", ToolTipIcon.Info);
+
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += delegate
+            {
+                this.BeginInvoke(new MethodInvoker(delegate()
+                {
+                    try
+                    {
+                        RolesResponse response = client.Request<RolesResponse>(request);
+                    }
+                    catch (OpenrestException e2)
+                    {
+                        loginError.Text = "Error logining in. Please check username and password.";
+                        this.Show();
+                        return;
+                    }
+                    catch (Exception e2)
+                    {
+                        loginError.Text = "Connection problem. " + e2.Message;
+                        this.Show();
+                        return;
+                    }
+
+                    Registry.SetValue(REGISTRY_NAME, USERNAME, username);
+                    Registry.SetValue(REGISTRY_NAME, PASSWORD, password);
+
+                    ordersSince = 0;
+                    toPrint.RemoveRange(0, toPrint.Count);
+
+                    loginpanel.Hide();
+                    loggedinpanel.Show();
+                    loggedIn = true;
+                    refreshTimer.Stop();
+                    refreshTimer.Start();
+                    printing = false;
+                    printNext();
+                }));
+            };
+            worker.RunWorkerAsync();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            Registry.SetValue(REGISTRY_NAME, USERNAME, "");
+            Registry.SetValue(REGISTRY_NAME, PASSWORD, "");
+
+            textBox2.Text = "";
+
+            loggedIn = false;
+            loggedinpanel.Hide();
+            loginpanel.Show();
+        }
+
+        private void Printers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Registry.SetValue(REGISTRY_NAME, REGISTRY_PRINTER_NAME, Printers.SelectedItem); 
+        }
+
+        private void ConfigurePrinter_Click(object sender, EventArgs e)
+        {
+            browser.ShowPrintDialog();
+        }
+
+        private void OpenSite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("chrome", "https://apps.openrest.com/");
+            }
+            catch (Exception e2)
+            {
+                try
+                {
+                    Process.Start("firefox", "https://apps.openrest.com/");
+                }
+                catch (Exception e3)
+                {
+                    try
+                    {
+                        Process.Start("https://apps.openrest.com/");
+                    }
+                    catch (Exception e4)
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
